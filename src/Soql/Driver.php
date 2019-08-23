49a7390e613010d69d97192801fd81fbc22ed429
Jefersson Nathan <malukenho.dev@gmail.com>
2019-08-23 11:36:27 +0200

    Driver: Do not allow empty username nor password

--- src/Soql/Driver.php
+++ src/Soql/Driver.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Codelicia\Soql;
 
+use Assert\Assertion;
+use Assert\AssertionFailedException;
 use Doctrine\DBAL\Driver\Connection;
 
 class Driver extends AbstractSoqlDriver
 {
+    /** @throws AssertionFailedException */
     public function connect(array $params, $username = null, $password = null, array $driverOptions = []) : Connection
     {
+        Assertion::notNull($username);
+        Assertion::notNull($password);
+
         return new SoqlConnection($params, (string) $username, (string) $password);
     }
 
