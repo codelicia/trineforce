660fd537894fdb081d956ebce4c056ae94aeb301
Jefersson Nathan <malukenho.dev@gmail.com>
2019-10-07 14:29:34 +0200

    Implements "delete" thought api
    To be honest, I didn't like that usage here, but it is less complicated
    then parsing the whole query and decide on which record to update, Once
    can also say that I could use criterias as a SELECT before doing the
    delete, but that is too much for Salesforce usecase.
    
    I've also fixed the "fetch" and "fetchAll" methods:
    
    - fetch(): will return only one MANDATORY result
    - fetchAll(): will return everything else
    
    Reviewed-by: Alexandre Eher <alexandre.eher@werkspot.nl>
    Signed-off-by: Jefersson Nathan <malukenho.dev@gmail.com>

--- src/Soql/ConnectionWrapper.php
+++ src/Soql/ConnectionWrapper.php
@@ -5,11 +5,37 @@ declare(strict_types=1);
 namespace Codelicia\Soql;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception\InvalidArgumentException;
+use function count;
+use function key;
+use function sprintf;
 
 class ConnectionWrapper extends Connection
 {
     public function createQueryBuilder() : QueryBuilder
     {
         return new QueryBuilder($this);
+    }
+
+    /** {@inheritDoc} */
+    public function delete($tableExpression, array $identifier, array $types = []) : void
+    {
+        if (empty($identifier)) {
+            throw InvalidArgumentException::fromEmptyCriteria();
+        }
+
+        $this->connect();
+
+        /** @var SoqlConnection $connection */
+        $connection = $this->_conn;
+        $http       = $connection->getHttpClient();
+
+        if (count($identifier) > 1) {
+            throw InvalidArgumentException::notSupported('It should have only one DELETE criteria.');
+        }
+
+        $param = $identifier['Id'] ?? (key($identifier) . '/' . $identifier[key($identifier)]);
+
+        $http->request('DELETE', sprintf('/services/data/v20.0/sobjects/%s/%s', $tableExpression, $param));
     }
 }
